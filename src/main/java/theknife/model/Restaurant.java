c1d6f3d93d596191c2ba9f6eaf37377c03160546
Cele-stino <celestino.resteghini@gmail.com>
2026-01-04 16:41:09 +0100

    Modifiche aggiungiDaRigaCsv

--- src/main/java/theknife/model/Restaurant.java
+++ src/main/java/theknife/model/Restaurant.java
@@ -1,58 +1,63 @@
 package theknife.model;
 
+import java.util.LinkedList;
+
 public class Restaurant {
     private String nome;
     private String citta;
     private String indirizzo;
-    private String Prezzo;
+    private double prezzo;
     private boolean delivery;
     private boolean booking;
-    private String tipoCucina;
+    private LinkedList<String> tipoCucina;
     private String nazione;
     private double latitudine;
     private double longitudine;
     private String website;
     private String link;
-    private String awards;
+    private double awards;
 
     public Restaurant() {
     }
 
     public Restaurant(String nome, String Nazione, String citta, String indirizzo,
-                      double Latitudine, double longitudine, String Prezzo,
-                      boolean delivery, boolean booking, String tipoCucina) {
+                      double latitudine, double longitudine, double prezzo,
+                      boolean delivery, boolean booking, LinkedList<String> tipoCucina, String website, String link, double awards) {
         this.nome = nome;
         this.nazione = Nazione;
         this.citta = citta;
         this.indirizzo = indirizzo;
-        this.latitudine = Latitudine;
+        this.latitudine = latitudine;
         this.longitudine = longitudine;
-        this.Prezzo = Prezzo;
+        this.prezzo = prezzo;
         this.delivery = delivery;
         this.booking = booking;
         this.tipoCucina = tipoCucina;
+        this.website = website;
+        this.link = link;
+        this.awards = awards;
     }
 
     // getter
     public String getNome() { return nome; }
     public String getCitta() { return citta; }
     public String getIndirizzo() { return indirizzo; }
-    public String getPrezzo() { return Prezzo; }
+    public double getPrezzo() { return prezzo; }
     public boolean isDelivery() { return delivery; }
     public boolean isBooking() { return booking; }
-    public String getTipoCucina() { return tipoCucina; }
+    public LinkedList<String> getTipoCucina() { return tipoCucina; }
     public String getNazione() { return nazione; }
     public double getLatitudine() { return latitudine; }
     public double getLongitudine() { return longitudine; }
     public String getWebsite() { return website; }
     public String getLink() { return link; }
-    public String getAwards() { return awards; }
+    public double getAwards() { return awards; }
 
     // setter
     public void setNome(String nome) { this.nome = nome; }
     public void setCitta(String citta) { this.citta = citta; }
     public void setIndirizzo(String indirizzo) { this.indirizzo = indirizzo; }
-    public void setPrezzo(String prezzo) { this.Prezzo = prezzo; }
+    public void setPrezzo(String prezzo) { this.prezzo = prezzo; }
     public void setDelivery(boolean delivery) { this.delivery = delivery; }
     public void setBooking(boolean booking) { this.booking = booking; }
     public void setTipoCucina(String tipoCucina) { this.tipoCucina = tipoCucina; }
