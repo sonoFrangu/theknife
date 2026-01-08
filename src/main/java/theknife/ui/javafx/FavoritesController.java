026ab27cb2607c5de869b6d41aa52f92301e5fa6
Tommaso-Ossuzio <tommasoossuzio@gmail.com>
2026-01-08 16:09:38 +0100

    Aggiunta JavaDoc by Tommaso Ossuzio

--- src/main/java/theknife/ui/javafx/FavoritesController.java
+++ src/main/java/theknife/ui/javafx/FavoritesController.java
@@ -8,6 +8,13 @@ import theknife.model.Ristorante;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
+/**
+ * Classe che si occupa della gestione dei ristoranti preferiti.
+ * @author Celestino Resteghini
+ * @author Matteo Franguelli
+ * version 2
+ *
+ */
 public class FavoritesController {
 
     @FXML private ListView<Ristorante> listaPreferiti;
@@ -19,6 +26,11 @@ public class FavoritesController {
     //TODO: stesso discorso di MyRestaurantController.java, prima devo avere un file da cui prendere i preferiti
     // se volete farlo e' uguale a MyReviewsController.java
     // fatemi sapere se bisogna fare anche quale "Elimina" e "Modifica"
+
+    /**
+     * Inizializza la lista vuota.
+     * @author Matteo Franguelli
+     */
     @FXML
     private void initialize() {
         // per ora la lista è vuota → solo grafica
@@ -26,6 +38,12 @@ public class FavoritesController {
         aggiornaMessaggioVuoto();
     }
 
+    /**
+     * Si occupa di aggiungere il ristorante passato ai preferiti.
+     * @param ristorante
+     * @author Celestino Resteghini
+     * @author Matteo Franguelli
+     */
     public void addFavorite(Ristorante ristorante) {
         if (ristorante == null) return;
         if (!preferiti.contains(ristorante)) {
@@ -34,6 +52,10 @@ public class FavoritesController {
         }
     }
 
+    /**
+     * Si occupa di mostrare l'etichetta che indica i ristoranti preferiti.
+     * @author Matteo Franguelli
+     */
     private void aggiornaMessaggioVuoto() {
         boolean nessunElemento = preferiti.isEmpty();
         etichettaVuota.setVisible(nessunElemento);
