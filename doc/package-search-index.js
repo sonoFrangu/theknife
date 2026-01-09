40ca2928e6ed5955602a45ab6eb177f35ab62f85
sonoFrangu <franguelli.matteo05@gmail.com>
2026-01-09 20:30:48 +0100

    JavaDoc caricata

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"it.unininsubria.theknifeui","l":"theknife"},{"m":"it.unininsubria.theknifeui","l":"theknife.model"},{"m":"it.unininsubria.theknifeui","l":"theknife.ui.javafx"}];updateSearchResults();
